bcd4748b33201d0c1cc64d4fdbab5787bdf1caae
Sydney <Sydneychristopherjnr@gmail.com>
2026-01-16 01:25:40 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `<main>
+    <header class="brand-name">
+      <img class="brand-logo"
+        src="assets/logo.svg"
+        alt="Logo"
+        aria-hidden="true"
+      />
+    </header>
+  </main>`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
